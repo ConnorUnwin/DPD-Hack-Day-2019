924ecc044ca1bff099b12708c8c868f02239f1ff
Connor Unwin <connor@Connors-iMac.home>
2019-05-17 16:46:59 +0100

    Final Version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Copyright of Connor Unwin 2019(C)
+
 // Game Variables
 var gameValues = ["scissors","paper","rock","lizard","spock"];
 var winningCombinations = [[0,1], [0,3], [1,2], [1,4], [2,3], [2,0], [3,4], [3,1], [4,2], [4,0]];
@@ -7,7 +8,6 @@ var gameValuesIcons = ["fa-hand-scissors", "fa-hand-paper", "fa-hand-rock","fa-h
 // Player 1 Variables
 var player1Status = document.getElementsByClassName("player1")[0];
 var player1Answer = document.getElementsByClassName("player1-answer")[0];
-
 
 // Player 2 Variables
 var player2Status = document.getElementsByClassName("player2")[0];
@@ -39,7 +39,6 @@ spockBtn = document.getElementById("spock").addEventListener("click", function()
   checkAnswers(getIndexValue(this.value), getIndexValue(gameValues[Math.floor(Math.random() * gameValues.length)]));
 });
 
-
 function clearPlayersStatus(answer1, answer2){
   // Player 1 Styles
   player1Status.classList.remove("loser");
@@ -60,7 +59,6 @@ function clearPlayersStatus(answer1, answer2){
   player2Status.classList.remove("draw");
 }
 
-
 function getIndexValue(answer) {
   // Check to make sure answer is in array.
   if (answer != -1){
@@ -72,56 +70,41 @@ function getIndexValue(answer) {
 
 function checkAnswers(answer1, answer2){
   if (answer1 === answer2){
-    // Player 1
+    // Player 1 Result
     player1Status.classList.add("draw");
     player1Status.innerHTML = "Draw!";
     player1Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer1] + "'></i>" + "<br>" + gameValues[answer1];
-    player1Answer.classList.add(gameValues[answer1]);
 
-    // Player 2
+    // Player 2 Result
     player2Status.classList.add("draw");
     player2Status.innerHTML = "Draw!";
     player2Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer2] + "'></i>" + "<br>" + gameValues[answer2];
-    player2Answer.classList.add(gameValues[answer2]);
-
-    // Debugging
-    console.log("It's a Draw!");
   } else {
     for (var i = 0; i < winningCombinations.length; i++){
         for (var k = 0; k < winningCombinations[i].length; k++){
           if (winningCombinations[i][k] === winningCombinations[i][0]){
             if(answer1 === winningCombinations[i][0] && answer2 === winningCombinations[i][1]){
-              //Player 1
+              //Player 1 Result
               player1Status.classList.add("winner");
               player1Status.innerHTML = "Winner";
               player1Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer1] + "'></i>" + "<br>" + gameValues[answer1];
-              player1Answer.classList.add(gameValues[answer1]);
 
-              //Player 2
+              //Player 2 Result
               player2Status.classList.add("loser");
               player2Status.innerHTML = "Loser";
               player2Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer2] + "'></i>" + "<br>" + gameValues[answer2];
-              player2Answer.classList.add(gameValues[answer2]);
-
-              // Debugging
-              console.log("[Player 1] used [" + gameValues[answer1] + "] and [Player 2] used [" + gameValues[answer2] + "]");
               break;
             }
             else if (answer1 === winningCombinations[i][1] && answer2 === winningCombinations[i][0]){
-              // Player 1
+              // Player 1 Result
               player1Status.classList.add("loser");
               player1Status.innerHTML = "Loser";
               player1Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer1] + "'></i>" + "<br>" + gameValues[answer1];
-              player1Answer.classList.add(gameValues[answer1]);
 
-              // Player 2
+              // Player 2 Result
               player2Status.classList.add("winner");
               player2Status.innerHTML = "Winner";
               player2Answer.innerHTML = "<i class='fas text-center fa-2x " + " " + " " + gameValuesIcons[answer2] + "'></i>" + "<br>" + gameValues[answer2];
-              player2Answer.classList.add(gameValues[answer2]);
-
-              // Debugging
-              console.log("[Player 2] used [" + gameValues[answer2] + "] and [Player 1] used [" + gameValues[answer1] + "]");
               break;
             }
           }
